3223fcf7bd24c023839dea5bbbf675808cf54e3e
klecio.batista <klecio.batista@interligados.com.br>
2024-08-21 19:03:07 -0300

    feature: arquivos para o crud

---
+++ challenge_codesh_laravel/app/Http/Controllers/FoodFactController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FoodFact;
+use Illuminate\Http\Request;
+
+class FoodFactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FoodFact $foodFact)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(FoodFact $foodFact)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, FoodFact $foodFact)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FoodFact $foodFact)
+    {
+        //
+    }
+}
