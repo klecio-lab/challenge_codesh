39b13f6c22bf874794daa4e129f1bec38d57a5af
klecio.batista <klecio.batista@interligados.com.br>
2024-08-21 19:17:59 -0300

    fix: deixando apennas os crus pedidos

--- challenge_codesh_laravel/database/migrations/2024_08_21_220016_create_food_facts_table.php
+++ challenge_codesh_laravel/database/migrations/2024_08_21_220016_create_food_facts_table.php
@@ -13,6 +13,29 @@ return new class extends Migration
     {
         Schema::create('food_facts', function (Blueprint $table) {
             $table->id();
+            $table->integer('code')->unique();
+            $table->enum('status', ['draft', 'trash', 'published']);
+            $table->timestamp('imported_t');
+            $table->string('url');
+            $table->string('creator');
+            $table->bigInteger('created_t');
+            $table->bigInteger('last_modified_t');
+            $table->string('product_name');
+            $table->string('quantity');
+            $table->string('brands');
+            $table->string('categories');
+            $table->string('labels')->nullable();
+            $table->string('cities')->nullable();
+            $table->string('purchase_places')->nullable();
+            $table->string('stores')->nullable();
+            $table->text('ingredients_text');
+            $table->string('traces')->nullable();
+            $table->string('serving_size');
+            $table->float('serving_quantity');
+            $table->integer('nutriscore_score');
+            $table->string('nutriscore_grade');
+            $table->string('main_category');
+            $table->string('image_url');
             $table->timestamps();
         });
     }
--- challenge_codesh_laravel/routes/api.php
+++ challenge_codesh_laravel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FoodFactController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/products', [FoodFactController::class, 'index']);
+Route::get('/products/{code}', [FoodFactController::class, 'show']);
+Route::put('/products/{code}', [FoodFactController::class, 'update']);
+Route::delete('/products/{code}', [FoodFactController::class, 'destroy']);
