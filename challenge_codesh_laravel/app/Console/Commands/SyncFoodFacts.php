98f4e3b6c5b07b51bf98d633b9ae5f33d8daf6fb
klecio.batista <klecio.batista@interligados.com.br>
2024-08-23 09:57:06 -0300

    fix: separando leitura do arquivo em partes de 100

--- challenge_codesh_laravel/app/Console/Commands/SyncFoodFacts.php
+++ challenge_codesh_laravel/app/Console/Commands/SyncFoodFacts.php
@@ -2,9 +2,11 @@
 
 namespace App\Console\Commands;
 
+use App\Jobs\ProcessFoodFactsByChunkJob;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\LazyCollection;
 
 class SyncFoodFacts extends Command
 {
@@ -21,10 +23,48 @@ class SyncFoodFacts extends Command
     {
         $fileNames = explode("\n", trim(Http::get($this->filesUrl)->body()));
 
-        foreach ($fileNames as $key => $fileName) {
+        foreach ($fileNames as $fileName) {
             $compressedJsonFoodFacts = file_get_contents($this->urlJsonFoodFacts . $fileName);
+
+            // Descompactar o conteúdo do GZIP
             $jsonFoodFactData = gzdecode($compressedJsonFoodFacts);
-            dd($jsonFoodFactData);
+
+            // Salvar o JSON descompactado no storage
+            $jsonFileName = pathinfo($fileName, PATHINFO_FILENAME);
+            $jsonFilePath = "food-facts/uncompressed/{$jsonFileName}";
+            Storage::put($jsonFilePath, $jsonFoodFactData);
+
+            // Obter o caminho completo do arquivo salvo (opcional)
+            $fileFullPath = Storage::path($jsonFilePath);
+
+            $this->dispatchFoodFactsByChunkJob($fileFullPath);
         }
+    }
+
+    public function dispatchFoodFactsByChunkJob($fileFullPath){
+        LazyCollection::make(function () use($fileFullPath) {
+            $filePath = $fileFullPath;
+            $file = fopen($filePath, 'r');
+
+            while ($line = fgets($file)) {
+                yield $line;
+            }
+
+            fclose($file);
+        })
+        ->chunk(2)
+        ->each(function ($linesChunk) {
+
+            $dataBatch = [];
+
+            foreach ($linesChunk as $line) {
+                $jsonData = json_decode($line, true);
+                dd($jsonData);
+                $dataBatch[] = $jsonData;
+            }
+            dd($dataBatch[]);
+            // Despacha um job para processar este batch
+            // ProcessFoodFactsByChunkJob::dispatch($dataBatch);
+        });
     }
 }
