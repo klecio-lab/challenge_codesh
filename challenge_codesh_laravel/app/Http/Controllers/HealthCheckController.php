de28d9136cdb5d8d211d5601804ec01e0ee57177
KlecioHenrique <85895969+klecio-lab@users.noreply.github.com>
2024-08-24 14:54:23 -0300

    fix: melhoria no retorno do status da api

--- challenge_codesh_laravel/app/Http/Controllers/HealthCheckController.php
+++ challenge_codesh_laravel/app/Http/Controllers/HealthCheckController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
@@ -21,6 +22,14 @@ class HealthCheckController extends Controller
         // Última vez que o cron foi executado
         $lastCronRun = Cache::get('last_cron_run', 'Nunca executado');
 
+        if($lastCronRun != 'Nunca executado'){
+            // Converte a data ISO para um objeto Carbon
+            $date = Carbon::parse($lastCronRun);
+
+            // Formata a data no padrão brasileiro
+            $lastCronRun = $date->format('H:i:s d/m/Y');
+        }
+
         // Tempo online e uso de memória
         $uptime = shell_exec('uptime');
         $memoryUsage = memory_get_usage();
@@ -29,7 +38,19 @@ class HealthCheckController extends Controller
             'database_connection' => $databaseConnection,
             'last_cron_run' => $lastCronRun,
             'uptime' => trim($uptime),
-            'memory_usage' => $memoryUsage,
+            'memory_usage' => $this->formatMemoryUsage($memoryUsage)
         ]);
+    }
+
+    public function formatMemoryUsage($sizeInBytes) {
+        $units = ['B', 'KB', 'MB', 'GB', 'TB'];
+        $unitIndex = 0;
+
+        while ($sizeInBytes >= 1024 && $unitIndex < count($units) - 1) {
+            $sizeInBytes /= 1024;
+            $unitIndex++;
+        }
+
+        return number_format($sizeInBytes, 2) . ' ' . $units[$unitIndex];
     }
 }
